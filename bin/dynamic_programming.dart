0ea9ef78f4f2c3bf90b758e3b45588949fe3fe59
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2022-02-06 13:18:37 +0500

    initial commit

---
+++ bin/dynamic_programming.dart
@@ -0,0 +1,5 @@
+import 'package:dynamic_programming/dynamic_programming.dart' as dynamic_programming;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dynamic_programming.calculate()}!');
+}
---
+++ test/dynamic_programming_test.dart
@@ -0,0 +1,8 @@
+import 'package:dynamic_programming/dynamic_programming.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
